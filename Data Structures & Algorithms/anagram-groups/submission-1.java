80ca08a849d26838e44760d0d70c56749cc7be54
10g1n <108683576+10g1n@users.noreply.github.com>
2026-07-14 15:43:17 +0300

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            String sortedStr = generateKey(str);
+
+            if (map.containsKey(sortedStr)) {
+                map.get(sortedStr).add(str);
+            } else {
+                List<String> list = new ArrayList<>();
+                list.add(str);
+                map.put(sortedStr, list);
+            }
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public String generateKey(String str) {
+        char[] chars = str.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
+    }
+}
